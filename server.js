57dccf40b5db47be9aa803fb288ed4ad73624335
yanataranets <yana.taranet.s@gmail.com>
2021-02-26 10:21:20 +0100

    first

--- server.js
+++ server.js
@@ -28,5 +28,5 @@ new Ignitor(require('@adonisjs/fold'))
 
 
 
-const port = process.env.PORT || 3000;
-app.listen(port, ()=> console.log('listening song on port 3000'));
+const port = process.env.PORT || 3333;
+app.listen(port, ()=> console.log('listening song on port '));
